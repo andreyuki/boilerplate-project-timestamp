bcd064452c403776fdb85d7f92c22f6073520eb8
Andre Ide <andreyukiide@gmail.com>
2024-07-13 01:40:59 -0300

    change for accept all strings

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
 app.get("/api", function(req, res){
   const now_timestamp = Date.now();
   const now_date = new Date(now_timestamp);
@@ -37,39 +36,65 @@ app.get("/api", function(req, res){
 });
 
 app.get("/api/:date", function(req, res){
-  const date = req.params.date
-  const regex_date = /\d{4}-\d{2}-\d{2}/;
+  let date
   const regex_unix = /^\d{1,16}$/;
+  const dateString = req.params.date;
 
-  if (regex_date.test(date)){
-    try{
-      const new_date = new Date(date);
-      const date_unix = Date.parse(new_date);
-      const date_utc = new_date.toUTCString();
-      res.json({
-        "unix":date_unix,
-        "utc":date_utc
-      });
-    }catch(e){
-      res.json({"error":e})
-    }
+  if (regex_unix.test(dateString)){
+    console.log(date)
+    date = new Date(parseInt(dateString));
+  } else {
+    date = new Date(dateString);
   }
-  else if (regex_unix.test(date)){
-    try{
-      const date_unix = date;
-      const now_date = new Date(parseInt(date_unix));
-      const date_utc = now_date.toUTCString();
-      res.json({
-        "unix":date_unix,
-        "utc":date_utc
-      });
-    }catch(e){
-      res.json({"error":e})
-    }
-  }else{
-    res.json({"error":"Invalid Date"})
+
+  if (isNaN(date.getTime())) {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
   }
+
 })
+
+
+
+
+// app.get("/api/:date", function(req, res){
+//   const date = req.params.date
+//   const regex_date = /\d{4}-\d{2}-\d{2}/;
+//   const regex_unix = /^\d{1,16}$/;
+
+//   if (regex_date.test(date)){
+//     try{
+//       const new_date = new Date(date);
+//       const date_unix = Date.parse(new_date);
+//       const date_utc = new_date.toUTCString();
+//       res.json({
+//         "unix":parseInt(date_unix),
+//         "utc":date_utc
+//       });
+//     }catch(e){
+//       res.json({"error":e})
+//     }
+//   }
+//   else if (regex_unix.test(date)){
+//     try{
+//       const date_unix = date;
+//       const now_date = new Date(parseInt(date_unix));
+//       const date_utc = now_date.toUTCString();
+//       res.json({
+//         "unix":parseInt(date_unix),
+//         "utc":date_utc
+//       });
+//     }catch(e){
+//       res.json({"error":e})
+//     }
+//   }else{
+//     res.json({"error":"Invalid Date"})
+//   }
+// })
 
 
 // Listen on port set in environment variable or default to 3000

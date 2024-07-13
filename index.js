12d591007d69fc49d5d8bd3fb756a1cb71dc912b
Andre Ide <andreyukiide@gmail.com>
2024-07-12 23:53:28 -0300

    /api endpoints

--- index.js
+++ index.js
@@ -25,6 +25,52 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get("/api", function(req, res){
+  const now_timestamp = Date.now();
+  const now_date = new Date(now_timestamp);
+  const formatted_date = now_date.toUTCString();
+
+  res.json({
+    "unix":now_timestamp,
+    "utc":formatted_date
+  });
+});
+
+app.get("/api/:date", function(req, res){
+  const date = req.params.date
+  const regex_date = /\d{4}-\d{2}-\d{2}/;
+  const regex_unix = /^\d{1,16}$/;
+
+  if (regex_date.test(date)){
+    try{
+      const new_date = new Date(date);
+      const date_unix = Date.parse(new_date);
+      const date_utc = new_date.toUTCString();
+      res.json({
+        "unix":date_unix,
+        "utc":date_utc
+      });
+    }catch(e){
+      res.json({"error":e})
+    }
+  }
+  else if (regex_unix.test(date)){
+    try{
+      const date_unix = date;
+      const now_date = new Date(parseInt(date_unix));
+      const date_utc = now_date.toUTCString();
+      res.json({
+        "unix":date_unix,
+        "utc":date_utc
+      });
+    }catch(e){
+      res.json({"error":e})
+    }
+  }else{
+    res.json({"error":"Invalid Date"})
+  }
+})
+
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
